33274f66a58ac6d3aec2bf6360d3a84200aa1dd9
Jordan Wunderlich <jordan.wunder@gmail.com>
2017-08-16 09:54:23 -0600

    Added authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Channel } from './channel';
+import { Router } from '@angular/router';
+import { AuthService } from './providers/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,27 @@ import { Channel } from './channel';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  CHANNELS: Channel[] = [
-    {title: 'Channel 1', desc: 'Desc 1'},
-    {title: 'Channel 2', desc: 'Desc 2'}
-  ];
-  title = 'Project';
+  private isLoggedIn: Boolean;
+  private user_displayName: String;
+  private user_email: String;
+  constructor(public authService: AuthService, private router: Router) {
+    this.authService.afAuth.authState.subscribe(
+      (auth) => {
+        if (auth == null) {
+          console.log("Logged out");
+          this.isLoggedIn = false;
+          this.user_displayName = '';
+          this.user_email = '';
+          this.router.navigate(['login']);
+        } else {
+          this.isLoggedIn = true;
+          this.user_displayName = auth.displayName;
+          this.user_email = auth.email;
+          console.log("Logged in");
+          console.log(auth);
+          this.router.navigate(['']);
+        }
+      }
+    );
+  }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
-import { Channel } from './channel';
+import { Channel } from './channel/channel';
+import { AuthService } from './providers/auth.service';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-NYG1omm4-TvrK_urfdJPC3Tb7oSjTN8",
+  authDomain: "whiteboarding-1ac2e.firebaseapp.com",
+  databaseURL: "https://whiteboarding-1ac2e.firebaseio.com",
+  storageBucket: "whiteboarding-1ac2e.appspot.com",
+};
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    Channel
+    Channel,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
